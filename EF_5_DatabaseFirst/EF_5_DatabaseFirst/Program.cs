e989f6209e9d20af1cb64d9ec8a945d205a7fe00
Eder Franco <francoheder@gmail.com>
2015-07-27 21:25:54 -0300

    Commit inicial dos estudos

---
+++ EF_5_DatabaseFirst/EF_5_DatabaseFirst/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_5_DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MeuBancoEntity())
+            {
+                var book = new titles() 
+                {                    
+                    title = "Intro to Entity Framework 5.0",
+                };
+
+                var book2 = new titles()
+                {
+                    title = "Intro to Entity Framework 5.0 - Part 2",
+                };
+
+                var publisher = new publishers() 
+                {
+                    pub_name = "Chéder Franco",
+                };
+
+                publisher.titles.Add(book);
+                publisher.titles.Add(book2);
+
+                db.publishers.Add(publisher);
+                
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
